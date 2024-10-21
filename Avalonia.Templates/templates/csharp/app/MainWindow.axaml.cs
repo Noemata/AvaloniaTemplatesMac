e640d6bbf359032a5f33cc5727cd92af046a6d11
Mario Pintaric <mario@noemata.org>
2024-10-21 10:53:23 -0400

    Added RelayCommand to app template.

--- Avalonia.Templates/templates/csharp/app/MainWindow.axaml.cs
+++ Avalonia.Templates/templates/csharp/app/MainWindow.axaml.cs
@@ -21,9 +21,13 @@ public partial class MainWindow : Window, INotifyPropertyChanged
         set => Set(ref _message, value);
     }
 
+    public ICommand DoIt => new RelayCommand(It);
+
     public MainWindow() => InitializeComponent();
 
     public void OnButtonClicked(object sender, RoutedEventArgs args) => Message = $"Hi number {++_count}!";
+
+    public void It() => Message = "It says hello.";
 
     #region PropertyChanged
     public new event PropertyChangedEventHandler? PropertyChanged;
@@ -46,3 +50,46 @@ public partial class MainWindow : Window, INotifyPropertyChanged
     protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
     #endregion
 }
+
+#region RelayCommand
+public class RelayCommand : ICommand
+{
+    private readonly Func<bool>? _canExecute;
+    private readonly Action _execute;
+    public event EventHandler? CanExecuteChanged;
+
+    public RelayCommand(Action execute) : this(execute, null) { }
+
+    public RelayCommand(Action execute, Func<bool>? canExecute)
+    {
+        _execute = execute ?? throw new ArgumentNullException(nameof(execute));
+        _canExecute = canExecute;
+    }
+
+    public bool CanExecute(object? parameter) => _canExecute == null || _canExecute();
+    public void Execute(object? parameter) => _execute();
+    public void RaiseCanExecuteChanged() => CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+}
+
+public class RelayCommand<T> : ICommand
+{
+    private readonly Func<bool>? _canExecute;
+    private readonly Action<T> _execute;
+    public event EventHandler? CanExecuteChanged;
+
+    public RelayCommand(Action<T> execute)
+        : this(execute, null)
+    {
+    }
+
+    public RelayCommand(Action<T> execute, Func<bool>? canExecute)
+    {
+        _execute = execute ?? throw new ArgumentNullException(nameof(execute));
+        _canExecute = canExecute;
+    }
+
+    public bool CanExecute(object? parameter) => _canExecute == null || _canExecute();
+    public void Execute(object? parameter) => _execute((T)parameter!);
+    public void RaiseCanExecuteChanged() => CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+}
+#endregion
